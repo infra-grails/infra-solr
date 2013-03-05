83ea51d0a1a6f40f20741d20d5b652e61fb42b9e
alari <name.alari@gmail.com>
2013-03-05 14:20:53 +0400

    added: solr config templates

--- InfraSolrGrailsPlugin.groovy
+++ InfraSolrGrailsPlugin.groovy
@@ -22,7 +22,7 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
